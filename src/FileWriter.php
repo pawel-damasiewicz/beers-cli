f3dd16a7d1931e65d2a0d9d8bdfc13b24bf9292c
Paweł Damasiewicz <pawel.damasiewicz@gmail.com>
2017-09-26 22:32:51 +0200

    modify: file writer - inject file path through constructor

--- src/FileWriter.php
+++ src/FileWriter.php
@@ -1,40 +1,23 @@
+
 <?php namespace PaulDam\BeersCli;
 
 class FileWriter implements WriterInterface
 {
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
     public function write(string $content): void
     {
-        $filename = $this->getFileName();
-
-        if (!$handle = fopen($filename, 'w')) {
-            throw new \RuntimeException("Cannot open file '$filename'.");
+        if (!$handle = fopen($this->path, 'w')) {
+            throw new \RuntimeException("Cannot open file '$this->path'.");
         }
 
         if (fwrite($handle, $content) === false) {
-            throw new \RuntimeException("Cannot write to file ($filename)");
+            throw new \RuntimeException("Cannot write to file ($path)");
         }
 
         fclose($handle);
-    }
-
-    public function withFilename(string $filename): WriterInterface
-    {
-        $copy = clone $this;
-        $copy->filename = $filename;
-
-        return $copy;
-    }
-
-    public function withStoragePath(string $path): WriterInterface
-    {
-        $copy = clone $this;
-        $copy->path = $path;
-
-        return $copy;
-    }
-
-    public function getFileName(): string
-    {
-        return $this->path . '/' . $this->filename;
     }
 }
