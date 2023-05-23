3b2274fc94324de86098681cd447e1528f0bb7cc
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2023-05-23 18:57:27 +0200

    Add MockBeerRepository

---
+++ src/Repository/MockBeerRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PaulDam\BeersCli\Repository;
+
+use PaulDam\BeersCli\Entity\BeerCollection;
+use PaulDam\BeersCli\Entity\BeerCollectionBuilder;
+use Symfony\Component\Yaml\Yaml;
+
+class MockBeerRepository implements BeerRepositoryInterface
+{
+    public function __construct(private readonly BeerCollectionBuilder $builder)
+    {
+    }
+
+    public function getBeers(): BeerCollection
+    {
+        $data = Yaml::parseFile('./storage/mockData.yaml');
+
+        return $this->builder->fromData($data);
+    }
+
+    public function filterByGlasswareId(int $glasswareId): BeerRepositoryInterface
+    {
+        return $this;
+    }
+}
